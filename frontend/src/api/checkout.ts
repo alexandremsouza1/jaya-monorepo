f07fe6120c7e90425ac37fd0e8e8cfc49b0a69de
Alexandre Magno <pedestaljunior@gmail.com>
2024-02-27 23:28:19 -0300

    as any

--- frontend/src/api/checkout.ts
+++ frontend/src/api/checkout.ts
@@ -1,19 +1,18 @@
 import useSWR from "swr";
 import useSWRMutation from "swr/mutation";
 import { fetcher } from "../lib/fetcher/clientFetcher";
-import { FlowExecutionResponse, IPaymentRequiredDataResponse, IPaymentStatusResponse } from "./types";
+import { FlowExecutionResponse, IPaymentRequiredDataResponse, IPaymentRunRequest, IPaymentStatusResponse } from "./types";
 
 export const RUN_PAYMENT_PATH = () => `/rest/payments`;
 
 export function useRunPayment() {
   const { trigger, data, isMutating, error } = useSWRMutation(
     RUN_PAYMENT_PATH(),
-    (url, { arg }) => fetcher<any>(url, { body: arg, method: "POST" }),
+    (url, { arg }) => fetcher<FlowExecutionResponse>(url, { body: arg, method: "POST" }),
     { throwOnError: true }
   );
-
   return {
-    doRunPayment: trigger,
+    doRunPayment: trigger as any,
     isLoading: isMutating,
     data,
     error,
