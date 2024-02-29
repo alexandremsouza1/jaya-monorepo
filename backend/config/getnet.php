7424742dbb8777c6984a1602cbf13a0b8f127b15
Alexandre Magno <pedestaljunior@gmail.com>
2024-02-28 22:22:11 -0300

    fix cors

--- backend/config/getnet.php
+++
@@ -1,4 +0,0 @@
-<?php
-return [
-    'org_id' => env('GETNET_ORG_ID', '1snn5n9w'),
-];
--- backend/config/twilio.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'sid' => env('TWILIO_SID'),
-    'token' => env('TWILIO_TOKEN'),
-    'from' => env('TWILIO_FROM'),
-];
